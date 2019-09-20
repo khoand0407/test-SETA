3e6d3721bc4bf2fe2f14fa967746f0c4da576f38
Nguyễn Minh Đức <nguyenminhduc@Nguyens-MacBook-Pro.local>
2019-09-20 19:02:50 +0700

    fix ex1

--- app/containers/HomePage/Exercise1.js
+++ app/containers/HomePage/Exercise1.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
-import { algorithm1 } from './caculator';
+import { algorithm1, sumNumTop2 } from './caculator';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -21,15 +21,20 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const input = ['a', 'ab', 'abc', 'cd', 'def'];
+const data1 = ['a', 'ab', 'abc', 'cd', 'def'];
+const data2 = [1, 4, 2, 3, 5];
 
-const result1 = algorithm1(input);
+const result1 = algorithm1(data1);
+const result2 = sumNumTop2(data2);
 
 export default function EX1() {
   const classes = useStyles();
 
-  const [input1] = React.useState(`[${input.join(', ')}]`);
+  const [input1] = React.useState(`[${data1.join(', ')}]`);
   const [output1] = React.useState(JSON.stringify(result1));
+
+  const [input2] = React.useState(`[${data2.join(', ')}]`);
+  const [output2] = React.useState(JSON.stringify(result2));
 
   return (
     <div>
@@ -53,6 +58,31 @@ export default function EX1() {
           multiline
           rowsMax="3"
           value={output1}
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+          disabled
+        />
+      </form>
+      <p>I.2</p>
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="outlined-multiline-flexible"
+          label="input"
+          multiline
+          rowsMax="3"
+          value={input2}
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+          disabled
+        />
+        <TextField
+          id="outlined-multiline-flexible"
+          label="output"
+          multiline
+          rowsMax="3"
+          value={output2}
           className={classes.textField}
           margin="normal"
           variant="outlined"
